69e5dbf2e3303514aee907a929314afb337b209b
atjpta <63082814+atjpta@users.noreply.github.com>
2022-04-11 17:54:55 +0700

    Revert "Haos branch"

--- AllClass/SqlAll.cs
+++ AllClass/SqlAll.cs
@@ -185,7 +185,9 @@ namespace QL_2.AllClass
             {
                 MessageBox.Show("Bạn đã nhập trùng Mã lô", "Thông báo cực căng!!!");
                 return false;
-            } 
+            }
+
+            
         }
 
         //tìm xem da có lô chưa
@@ -241,145 +243,5 @@ namespace QL_2.AllClass
                 return false;
             }
         }
-
-        /// <summary>
-        /// 
-        /// San pham
-        /// 
-        /// </summary>
-        /// <returns></returns>
-
-
-        public List<SanPham> GetAll_SanPham()
-        {
-            List<SanPham> sanPhams = new List<SanPham>();
-            cmd.Connection = con;
-            cmd.CommandText =
-                "select *" +
-                "from sanpham";
-            MySqlDataReader reader = cmd.ExecuteReader();
-
-            if (reader.HasRows)
-            {
-                while (reader.Read())
-                {
-                    SanPham sanPham = new SanPham
-                    (
-                        reader.GetInt32("id_sp"),
-                        reader.GetInt32("id_nv"),
-                        reader.GetString("malo"),
-                        reader.GetString("tensp"),
-                        reader.GetString("don_vi_tinh"),
-                        reader.GetInt32("so_luong_ton_kho"),
-                        (byte[])reader["img"]
-                     );
-
-                    sanPhams.Add(sanPham);
-                }
-            }
-            reader.Close();
-
-            return sanPhams;
-        }
-
-        public SanPham Find_SanPham(int maSP)
-        {
-            SanPham SanPham;
-            cmd.Connection = con;
-            cmd.CommandText =
-                "select * " +
-                "from sanpham " +
-                "where id_sp = " + maSP;
-            MySqlDataReader reader = cmd.ExecuteReader();
-
-            if (reader.HasRows)
-            {
-                while (reader.Read())
-                {
-                    SanPham = new SanPham
-                    (
-                        reader.GetInt32("id_sp"),
-                        reader.GetInt32("id_nv"),
-                        reader.GetString("malo"),
-                        reader.GetString("tensp"),
-                        reader.GetString("don_vi_tinh"),
-                        reader.GetInt32("so_luong_ton_kho"),
-                        (byte[])reader["img"]
-                     );
-                    reader.Close();
-                    return SanPham;
-                }
-            }
-            reader.Close();
-            return null;
-        }
-
-        public bool Add_SanPham(SanPham sanPham)
-        {
-            if (Find_Lo(sanPham.Id_san_pham.ToString()) == null)
-            {
-                cmd.Connection = con;
-                cmd.CommandText =
-                   "insert into sanpham( `ID_NV`, `MALO`, `TENSP`, `DON_VI_TINH`, `SO_LUONG_TON_KHO`, img) " +
-                   "values('" 
-                   + sanPham.Id_nhan_vien.ToString() + "', "
-                   + "'" + sanPham.Malo + "', "
-                   + "'" + sanPham.Ten_san_pham + "', "
-                   + "'" + sanPham.Don_vi_tinh + "', "
-                   + "'" + sanPham.So_luong_ton_kho + "', "
-                   + "@img)";
-
-                cmd.Parameters.Add(new MySqlParameter("@img", sanPham.Img));
-                try
-                {
-                    MySqlDataReader reader = cmd.ExecuteReader();
-                    reader.Close();
-                    return true;
-
-                }
-                catch (MySqlException ex)
-                {
-                    MessageBox.Show(ex.Message, "Thông báo cực căng!");
-                    return false;
-                }
-            }
-            else
-            {
-                MessageBox.Show("Bạn đã nhập trùng Mã Sản Phẩm", "Thông báo cực căng!");
-                return false;
-            }
-        }
-
-        public bool Update_SanPham(SanPham sanPham)
-        {
-            cmd.Connection = con;
-            cmd.CommandText =
-               "update sanpham " 
-               + "set "
-               + "id_nv = '" + sanPham.Id_nhan_vien.ToString() + "', "
-               + "malo = '" + sanPham.Malo + "', "
-               + "tensp = '" + sanPham.Ten_san_pham + "', "
-               + "don_vi_tinh = '" + sanPham.Don_vi_tinh + "', "
-               + "so_luong_ton_kho = '" + sanPham.So_luong_ton_kho + "', "
-               + "img = @img " 
-               + "where id_sp = " + sanPham.Id_san_pham;
-
-            cmd.Parameters.Add(new MySqlParameter("@img", sanPham.Img));
-
-            try
-            {
-                MySqlDataReader reader = cmd.ExecuteReader();
-                reader.Close();
-                return true;
-
-            }
-            catch (MySqlException ex)
-            {
-                MessageBox.Show(ex.Message, "Thông báo cực căng!");
-
-                return false;
-            }
-
-        }    
     }
 }
